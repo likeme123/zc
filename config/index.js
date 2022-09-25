4c35c948d94edc45c0e8587953d83a5080c99287
likeme123 <114336958+likeme123@users.noreply.github.com>
2022-09-25 18:46:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: 'opP5X67Q2gFlzzyBMmuE8TaxGAtQ',
+  APP_SECRET: 'b0a0a62bb8a7b61f08f67ae24af66af9',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
